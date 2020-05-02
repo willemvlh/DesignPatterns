47dd589d2714dbbfe9c5a9230e4eb6f0a0bf3eb6
Willem Van Lishout <willem.van.lishout@mediagenix.tv>
2020-05-02 14:53:22 +0200

    initial

---
+++ Models/Computer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Models
+{
+    public abstract class Computer
+    {
+        public string Manufacturer { get; set; }
+        public int RamSize { get; set; }
+        public int HardDiskSize { get; set; }
+    }
+
+    public class Laptop : Computer
+    {
+
+    }
+
+    public class DesktopComputer : Computer
+    {
+
+    }
+}
